73913aa61491f5c3dfd741e0e04bd69a90979e93
Andrew Molis <andrewmolis@gmail.com>
2022-09-23 10:49:02 -0700

    Initial setup

---
+++ LovokHandleInternal.cpp
@@ -0,0 +1,9 @@
+#include "lovok.h"
+
+enum LovokStatusCode {
+    SUCCESS = 0,
+};
+
+typedef struct LovokHandleInternal {
+    // TODO add later
+} *LOVOK_HANDLE;
---
+++ lovok.cpp
@@ -0,0 +1,3 @@
+#include "lovok.h"
+
+#include <iostream>
