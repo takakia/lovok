8ef115f2f731c57d9c3e92bc6a8b6d705f00f9bc
Andrew Molis <56096009+a-molis@users.noreply.github.com>
2022-09-27 11:04:30 -0700

    Initial setup for project structure and classes (#8)
    * Initial setup
    
    * Moved files around and added box classes
    
    * Updated formatting

---
+++ src/box.h
@@ -0,0 +1,15 @@
+//
+//
+#ifndef LOVOK_BOX_H
+#define LOVOK_BOX_H
+
+
+#include <string>
+
+class box {
+public:
+    int size;
+    std::string name;
+};
+
+#endif //LOVOK_BOX_H
---
+++ src/lovok_handle_internal.cpp
@@ -0,0 +1,9 @@
+#include "../include/lovok.h"
+
+enum LovokStatusCode {
+    SUCCESS = 0,
+};
+
+typedef struct LovokHandleInternal {
+    // TODO add later
+} *LOVOK_HANDLE;
