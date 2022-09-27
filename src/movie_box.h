30a0c5245523eb1c07f87374a335a97a4e7a1d66
Yamack <yamackarakus@berkeley.edu>
2022-09-27 13:50:28 -0700

    added the classes for moov, mvhd, and mfra boxes

---
+++ src/movie_box.h
@@ -0,0 +1,16 @@
+#include "box.h"
+#include "movie_header_box.h"
+
+#ifndef LOVOK_MOVIE_BOX_H
+#define LOVOK_MOVIE_BOX_H
+
+class MovieBox : public box {
+public:
+
+    MovieBox(int s, std::string n) {
+        size = s;
+        name = std::move(n);
+    }
+};
+
+#endif //LOVOK_MOVIE_BOX_H
