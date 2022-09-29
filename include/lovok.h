fba2afe7ad3f9ec28c1175a3a15b6a7aba27a99f
Sam <sym2015@gmail.com>
2022-09-29 13:05:23 -0700

    Added extern C in public header

--- include/lovok.h
+++ include/lovok.h
@@ -1,5 +1,8 @@
 #ifndef LOVOK_LOVOK_H
 #define LOVOK_LOVOK_H
+#if defined(__cplusplus)
+extern "C" {
+#endif
 
 typedef struct LovokHandleInternal *LOVOK_HANDLE;
 
@@ -7,4 +10,7 @@ LOVOK_HANDLE Lovok_create();
 
 void Lovok_destroy(LOVOK_HANDLE);
 
+#if defined(__cplusplus)
+}
+#endif
 #endif //LOVOK_LOVOK_H
