82af1080461df8480e93a585cd755a8be6e5d4f8
Sam <sym2015@gmail.com>
2022-10-24 12:01:08 -0700

    Added stubs for parsing minf sub box, filled in parseMinf

--- src/boxes/mdia_sub_boxes.cpp
+++ src/boxes/mdia_sub_boxes.cpp
@@ -1,5 +1,6 @@
 #include "../lovok_handle_internal.h"
 #include "mdia_sub_boxes.h"
+#include "minf_sub_boxes.h"
 
 LovokStatusCode ParseMdhd(FileWrapper * fileWrapper, uint64_t length) {
     return SUCCESS;
@@ -17,6 +18,26 @@ LovokStatusCode ParseElng(FileWrapper * fileWrapper, uint64_t length) {
 }
 
 LovokStatusCode ParseMinf(FileWrapper * fileWrapper, uint64_t length) {
-    return SUCCESS;
-    // todo if this is involved in an exploit
+    LovokStatusCode parseResults = ParseBoxes(fileWrapper,
+                                              length,
+                                              [&fileWrapper] (const Box &header) -> LovokStatusCode {
+          LovokStatusCode result = SUCCESS;
+          if (!strcmp(header.name, "vmhd")) {
+              result = ParseVmhd(fileWrapper, header.size);
+          } else if (!strcmp(header.name, "smhd")) {
+              result = ParseSmhd(fileWrapper, header.size);
+          } else if (!strcmp(header.name, "hmhd")) {
+              result = ParseHmhd(fileWrapper, header.size);
+          } else if (!strcmp(header.name, "sthd")) {
+              result = ParseSthd(fileWrapper, header.size);
+          } else if (!strcmp(header.name, "nmhd")) {
+              result = ParseNmhd(fileWrapper, header.size);
+          } else if (!strcmp(header.name, "dinf")) {
+              result = ParseDinf(fileWrapper, header.size);
+          } else if (!strcmp(header.name, "dref")) {
+              result = ParseDref(fileWrapper, header.size);
+          }
+          return result;
+      });
+    return parseResults;
 }
---
+++ src/boxes/minf_sub_boxes.cpp
@@ -0,0 +1,37 @@
+#include "../lovok_handle_internal.h"
+#include "minf_sub_boxes.h"
+
+LovokStatusCode ParseVmhd(FileWrapper * fileWrapper, uint64_t length) {
+    return SUCCESS;
+    // todo if this is involved in an exploit
+}
+
+LovokStatusCode ParseSmhd(FileWrapper * fileWrapper, uint64_t length) {
+    return SUCCESS;
+    // todo if this is involved in an exploit
+}
+
+LovokStatusCode ParseHmhd(FileWrapper * fileWrapper, uint64_t length) {
+    return SUCCESS;
+    // todo if this is involved in an exploit
+}
+
+LovokStatusCode ParseSthd(FileWrapper * fileWrapper, uint64_t length) {
+    return SUCCESS;
+    // todo if this is involved in an exploit
+}
+
+LovokStatusCode ParseNmhd(FileWrapper * fileWrapper, uint64_t length) {
+    return SUCCESS;
+    // todo if this is involved in an exploit
+}
+
+LovokStatusCode ParseDinf(FileWrapper * fileWrapper, uint64_t length) {
+    return SUCCESS;
+    // todo if this is involved in an exploit
+}
+
+LovokStatusCode ParseDref(FileWrapper * fileWrapper, uint64_t length) {
+    return SUCCESS;
+    // todo if this is involved in an exploit
+}
