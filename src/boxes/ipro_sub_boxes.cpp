cce8578aa8af76f319eac6642fdf7f6c4230957b
Sam <sym2015@gmail.com>
2022-10-28 15:47:25 -0700

    Added stubs for parsing ipro and below, filled in parseIpro and respective sub boxes

---
+++ src/boxes/ipro_sub_boxes.cpp
@@ -0,0 +1,23 @@
+#include "../lovok_handle_internal.h"
+#include "ipro_sub_boxes.h"
+#include "sinf_sub_boxes.h"
+
+LovokStatusCode ParseSinf(FileWrapper *fileWrapper, uint64_t length, uint64_t byteOffset) {
+    byteOffset += 8;
+    length -= 8;
+    LovokStatusCode parseResults = ParseBoxes(fileWrapper,
+                                              length,
+                                              byteOffset,
+                                              [&fileWrapper] (const Box &header, uint64_t byteOffset) -> LovokStatusCode {
+          LovokStatusCode result = SUCCESS;
+          if (!strcmp(header.name, "frma")) {
+              result = ParseFrma(fileWrapper, header.size, byteOffset);
+          } else if (!strcmp(header.name, "schm")) {
+              result = ParseSchm(fileWrapper, header.size, byteOffset);
+          } else if (!strcmp(header.name, "schi")) {
+              result = ParseSchi(fileWrapper, header.size, byteOffset);
+          }
+          return result;
+      });
+    return parseResults;
+}
---
+++ src/boxes/ipro_sub_boxes.h
@@ -0,0 +1,12 @@
+#include "box.h"
+#include "../lovok_handle_internal.h"
+#include "../include/lovok.h"
+#include "io/file_io.h"
+#include <functional>
+
+#ifndef LOVOK_IPRO_SUB_BOXES_H
+#define LOVOK_IPRO_SUB_BOXES_H
+
+LovokStatusCode ParseSinf(FileWrapper *, uint64_t, uint64_t);
+
+#endif //LOVOK_IPRO_SUB_BOXES_H
