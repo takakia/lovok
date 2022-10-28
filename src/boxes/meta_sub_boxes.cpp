1d2046493467f81038bc6ef8a117a1921d9b0b16
Sam <sym2015@gmail.com>
2022-10-28 16:07:20 -0700

    Filled in last stubs and methods for parse file level meta box and sub boxes

--- src/boxes/meta_sub_boxes.cpp
+++ src/boxes/meta_sub_boxes.cpp
@@ -2,6 +2,7 @@
 #include "meta_sub_boxes.h"
 #include "dinf_sub_boxes.h"
 #include "ipro_sub_boxes.h"
+#include "fiin_sub_boxes.h"
 
 LovokStatusCode ParseHdlr(FileWrapper *fileWrapper, uint64_t length, uint64_t byteOffset) {
     return SUCCESS;
@@ -66,8 +67,23 @@ LovokStatusCode ParsePitm(FileWrapper *fileWrapper, uint64_t length, uint64_t by
 }
 
 LovokStatusCode ParseFiin(FileWrapper *fileWrapper, uint64_t length, uint64_t byteOffset) {
-    return SUCCESS;
-    //todo if this is involved in an exploit
+    byteOffset += 8;
+    length -= 8;
+    LovokStatusCode parseResults = ParseBoxes(fileWrapper,
+                                              length,
+                                              byteOffset,
+                                              [&fileWrapper] (const Box &header, uint64_t byteOffset) -> LovokStatusCode {
+          LovokStatusCode result = SUCCESS;
+          if (!strcmp(header.name, "paen")) {
+              result = ParsePaen(fileWrapper, header.size, byteOffset);
+          } else if (!strcmp(header.name, "segr")) {
+              result = ParseSegr(fileWrapper, header.size, byteOffset);
+          } else if (!strcmp(header.name, "gitn")) {
+              result = ParseGitn(fileWrapper, header.size, byteOffset);
+          }
+          return result;
+      });
+    return parseResults;
 }
 
 LovokStatusCode ParseIdat(FileWrapper *fileWrapper, uint64_t length, uint64_t byteOffset) {
---
+++ src/boxes/paen_sub_boxes.cpp
@@ -0,0 +1,17 @@
+#include "../lovok_handle_internal.h"
+#include "paen_sub_boxes.h"
+
+LovokStatusCode ParseFire(FileWrapper *fileWrapper, uint64_t length, uint64_t byteOffset) {
+    return SUCCESS;
+    //todo if this is involved in an exploit
+}
+
+LovokStatusCode ParseFpar(FileWrapper *fileWrapper, uint64_t length, uint64_t byteOffset) {
+    return SUCCESS;
+    //todo if this is involved in an exploit
+}
+
+LovokStatusCode ParseFecr(FileWrapper *fileWrapper, uint64_t length, uint64_t byteOffset) {
+    return SUCCESS;
+    //todo if this is involved in an exploit
+}
