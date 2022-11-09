5152d0ca44356ea40e6a32fa14b2dca6a4c8f7fd
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2022-11-09 14:54:23 -0800

    Error handling for handle initializer
    - Bubble up malloc failure and file open failure cases
    - Make destroy function a no-op when called with NULL, following free() and fclose()
      conventions.

--- src/lovok.cpp
+++ src/lovok.cpp
@@ -6,19 +6,33 @@
 
 LOVOK_HANDLE Lovok_create(const char *name) {
     LOVOK_HANDLE handle = NULL;
+
     handle = (LOVOK_HANDLE) malloc(sizeof(LOVOK_HANDLE_INTERNAL));
-    handle->wrapper = FileWrapper_Open(name);
+    if (handle)
+    {
+        handle->wrapper = FileWrapper_Open(name);
+        if (handle->wrapper)
+            return handle;
+
+        free(handle);
+        handle = nullptr;
+    }
+
     return handle;
 }
 
 LOVOK_HANDLE Lovok_create_by_handle(FileWrapper *wrapper) {
     LOVOK_HANDLE handle = NULL;
     handle = (LOVOK_HANDLE) malloc(sizeof(LOVOK_HANDLE_INTERNAL));
-    handle->wrapper = wrapper;
+    if (handle)
+        handle->wrapper = wrapper;
     return handle;
 }
 
 void Lovok_destroy(LOVOK_HANDLE h) {
+    if (!h)
+        return;
+
     FileWrapper_Close(h->wrapper);
     free(h);
 }
