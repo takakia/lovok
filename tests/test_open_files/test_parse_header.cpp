1d80bc85d93d39d22f869c3e0815cf5fd4ae3a86
Andrew Molis <andrewmolis@gmail.com>
2022-11-17 13:58:14 -0800

    Updated formatting

--- tests/test_open_files/test_parse_header.cpp
+++ tests/test_open_files/test_parse_header.cpp
@@ -6,9 +6,9 @@ int main(int argc, char **argv) {
     std::cout << "Parsing" << name << std::endl;
     LOVOK_HANDLE handle = Lovok_create(name);
     if (!handle) {
-      std::cout << "Error validating opening file" << std::endl;
+      std::cout << "Error  opening file" << std::endl;
       return 1;
-    }  
+    }
     LovokStatusCode valid = valid_mp4(handle);
     if (valid != SUCCESS) {
         std::cout << "Error validating mp4 file" << std::endl;
