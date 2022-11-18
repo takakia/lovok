6ffa6961010be40b91775eec86d80ea0c1a55b0b
Andrew Molis <56096009+a-molis@users.noreply.github.com>
2022-11-18 11:32:36 -0800

    Fixes seg fault when tests are run outside of ctest (#73)
    * Fixed seg fault for tests
    
    * Updated formatting
    
    * Updated formatting

--- tests/test_open_files/test_parse_header.cpp
+++ tests/test_open_files/test_parse_header.cpp
@@ -5,6 +5,10 @@ int main(int argc, char **argv) {
     const char *name = "test_data/test_recording0.mp4";
     std::cout << "Parsing" << name << std::endl;
     LOVOK_HANDLE handle = Lovok_create(name);
+    if (!handle) {
+      std::cout << "Error opening file" << std::endl;
+      return 1;
+    }
     LovokStatusCode valid = valid_mp4(handle);
     if (valid != SUCCESS) {
         std::cout << "Error validating mp4 file" << std::endl;
