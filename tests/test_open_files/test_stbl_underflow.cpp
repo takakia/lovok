e2d76b3c2de75499e5b9b9124bf2931b07826e54
Yamack <yamackarakus@berkeley.edu>
2022-12-04 17:34:57 -0800

    added an underflow test, generated underflow and overflow test files

---
+++ tests/test_open_files/test_stbl_underflow.cpp
@@ -0,0 +1,28 @@
+//
+// Created by yamac on 12/4/2022.
+//
+
+#include <iostream>
+#include "lovok.h"
+
+int main(int argc, char **argv) {
+    const char *name = "test_data/test_sample_exploit_stsc_underflow.mp4";
+    std::cout << "Parsing" << name << std::endl;
+    FileWrapper *wrapper = FileWrapper_Open(name);
+    if (!wrapper) {
+        std::cout << "Error opening file" << std::endl;
+        return 1;
+    }
+    LOVOK_HANDLE handle = Lovok_create_by_handle(wrapper);
+    if (!handle) {
+        std::cout << "Error opening file" << std::endl;
+        return 1;
+    }
+    LovokStatusCode valid = valid_mp4(handle);
+    if (valid != INVALID_FILE) {
+        std::cout << "Error validating mp4 file" << std::endl;
+        return 1;
+    }
+    Lovok_destroy(handle);
+    return 0;
+}
